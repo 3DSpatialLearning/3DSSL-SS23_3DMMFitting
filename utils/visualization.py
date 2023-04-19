b3247549ba5d1c748a046bef7252d1873cbf57f7
WeiXiao <wei.xiao.xia@estudiantat.upc.edu>
2023-04-19 18:02:32 +0200

    visualization

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,10 +1,23 @@
+import numpy as np
+import pyvista as pv
 
-def visualize_points():
-    landmarks = np.load(LANDMARKS_FILE)
-    points = np.load(POINTS_FILE)
+
+def visualize_points(
+    landmarks_file_path: str,
+    points_file_path: str
+):
+    landmarks = np.load(landmarks_file_path)
+    points = np.load(points_file_path)
     pv_landmarks = pv.PolyData(landmarks)
     pv_points = pv.PolyData(points)
     plotter = pv.Plotter()
     plotter.add_mesh(pv_landmarks, color='red', point_size=5)
     plotter.add_mesh(pv_points, color='green', opacity=0.1, point_size=1)
     plotter.show()
+
+
+if __name__ == "__main__":
+    visualize_points(
+        landmarks_file_path="./data/toy_task/team1_landmarks.npy",
+        points_file_path="./data/toy_task/team1_points.npy"
+    )
