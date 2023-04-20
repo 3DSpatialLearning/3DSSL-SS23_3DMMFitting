3bab9898cf9ee84caa264ccb616ace3f518df353
ypan98 <yimin.pan.123@gmail.com>
2023-04-20 13:06:38 +0200

    toy task: refactor

--- toy_example/main.py
+++ toy_example/main.py
@@ -1,45 +1,23 @@
-import numpy as np
-import pyvista as pv
 import torch
 import torch.nn as nn
 from flame.FLAME import FLAME
 from flame.config import get_config
 from pytorch3d.loss import chamfer_distance
+from utils.visualization import visualize_points
+from utils.data import dict_tensor_to_np, load_data
 
 LANDMARKS_FILE = "./data/team1_landmarks.npy"
 POINTS_FILE = "./data/team1_points.npy"
 NORMALS_FILE = "./data/team1_normals.npy"
 
-def load_data():
-    landmarks = np.load(LANDMARKS_FILE)
-    landmarks /= 1000
-    points = np.load(POINTS_FILE)
-    points /= 1000
-    normals = np.load(NORMALS_FILE)
-    data = {
-        'landmarks': landmarks,
-        "points": points,
-        "normals": normals
-    }
-    return data
-
-def visualize_points(data: dict[str, np.ndarray]):
-    pv_landmarks = pv.PolyData(data["landmarks"])
-    pv_points = pv.PolyData(data["points"])
-    plotter = pv.Plotter()
-    plotter.add_mesh(pv_landmarks, color='red', point_size=5)
-    plotter.add_mesh(pv_points, color='green', opacity=0.1, point_size=1)
-    plotter.show()
-
 def visualize_points_to_fit():
-    data = load_data()
+    data = load_data(1/1000, LANDMARKS_FILE, POINTS_FILE, NORMALS_FILE)
     visualize_points(data)
 
 def fitting(data:dict, steps: int = 100, lr: float = 1e-3, wd: float = 1e-4):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     for k, v in data.items():
         data[k] = torch.from_numpy(v).float().unsqueeze(0).to(device)
-
     config = get_config()
     flame_model = FLAME(config)
     flame_model.to(device)
@@ -56,7 +34,7 @@ def fitting(data:dict, steps: int = 100, lr: float = 1e-3, wd: float = 1e-4):
         lr=lr,
         weight_decay=wd
     )
-    # optimize for pose
+    # optimize for pose (alternatively use procrustes)
     pose.requires_grad = True
     exp.requires_grad = False
     shape.requires_grad = False
@@ -71,28 +49,31 @@ def fitting(data:dict, steps: int = 100, lr: float = 1e-3, wd: float = 1e-4):
     exp.requires_grad = True
     shape.requires_grad = True
     for step in range(steps):
-        vertices, _ = flame_model(shape_params=shape, expression_params=exp, pose_params=pose)
+        vertices, landmarks3d = flame_model(shape_params=shape, expression_params=exp, pose_params=pose)
         loss, _ = chamfer_distance(data["points"], vertices)
         shape_optimizer.zero_grad()
         loss.backward()
         shape_optimizer.step()
-    return shape, exp, pose, vertices, landmarks3d
+    dict_in_tensor = {
+        "points": vertices,
+        "landmarks": landmarks3d,
+        "pose": pose,
+        "shape": shape,
+        "exp": exp
+    }
+    dict_in_np = dict_tensor_to_np(dict_in_tensor)
+    return dict_in_np
 
 def visualize_fitted_flame():
     data = load_data()
-    shape, exp, pose, out_points, out_landmarks = fitting(data=data, steps=200)
-    shape = shape.detach().cpu().numpy().squeeze()
-    exp = exp.detach().cpu().numpy().squeeze()
-    pose = pose.detach().cpu().numpy().squeeze()
-    print("pose:", pose)
-    print("shape:", shape)
-    print("exp:", exp)
-    out_data = {
-        "landmarks": out_landmarks.detach().cpu().numpy().squeeze(),
-        "points": out_points.detach().cpu().numpy().squeeze()
-    }
-    visualize_points(out_data)
+    fitted_data = fitting(data=data, steps=200)
+    for k, v in fitted_data.items():
+        fitted_data[k] = v.squeeze()
+    print("pose:", fitted_data["pose"])
+    print("shape:", fitted_data["shape"])
+    print("exp:", fitted_data["exp"])
+    visualize_points(fitted_data)
 
 if __name__ == '__main__':
-    # visualize_points_to_fit()
-    visualize_fitted_flame()
+    visualize_points_to_fit()
+    # visualize_fitted_flame()
