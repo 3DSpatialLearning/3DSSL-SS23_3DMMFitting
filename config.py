1a090e9306864f486b315c205b3048de63a4db3d
ypan98 <yimin.pan.123@gmail.com>
2023-06-20 21:04:22 +0200

    chore: expression param hint

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ parser.add_argument(
     '--tex_params',
     type=int,
     default=100,
-    help='the number of expression parameters'
+    help='the number of texture parameters'
 )
 
 parser.add_argument(
